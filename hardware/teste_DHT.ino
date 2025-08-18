53e81ac95e793789293c8130b3c273204a03b1c7
Rafaela Savaris <savarisf.rafaela@gmail.com>
2025-08-18 17:26:52 -0300

    adicao de infos sobre dht22 e comentarios no codigo

--- hardware/teste_DHT.ino
+++ hardware/teste_DHT.ino
@@ -1,23 +1,33 @@
-// Adafruit Unified Sensor Library: https://github.com/adafruit/Adafruit_Sensor
-
-#include <Adafruit_Sensor.h>                       // Biblioteca DHT Sensor Adafruit 
+// Teste do Sensor DHT11 ou DHT22 com as bibliotecas:
+// - Adafruit Sensor Library
+// - DHT Sensor Library - Adafruit
+// - Adafruit DHT Unified
+#include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-// selecione um sensor, retirando o comentário - duas barras
-#define DHTTYPE    DHT11                           // Sensor DHT11
-//#define DHTTYPE      DHT22                       // Sensor DHT22 ou AM2302
+#define DHTTYPE DHT11
+//#define DHTTYPE DHT22
 
-#define DHTPIN 2                                   // Pino do Arduino conectado no Sensor(Data) 
-DHT_Unified dht(DHTPIN, DHTTYPE);                  // configurando o Sensor DHT - pino e tipo
-uint32_t delayMS;                                  // variável para atraso no tempo
+#define DHTPIN 2
+
+// Cria um objeto do tipo DHT_Unified, o qual contém dois sub-objetos:
+// um para temperatura e outro para umidade.
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+// Variável para armazenar o delay entre as leituras
+uint32_t delayMS;
 
 void setup()
 {
+  // Inicializa o sensor DHT e a comunicação serial
   Serial.begin(9600);
-  dht.begin();                                    // inicializa a função
+  dht.begin();
+
   Serial.println("Usando o Sensor DHT");
+  // Cria um objeto sensor_t, que é uma struct, a qual contém informações sobre o sensor.
   sensor_t sensor;
+
   dht.temperature().getSensor(&sensor);
   Serial.println("------------------------------------");
   Serial.println("Temperatura");
@@ -36,32 +46,40 @@ void setup()
   Serial.print  ("Resolucao:   "); Serial.print(sensor.resolution); Serial.println("%");
   Serial.println("------------------------------------");
   
-  delayMS = sensor.min_delay / 1000;            // define o atraso entre as leituras
+  // Calcula o delay entre as leituras baseado no tempo mínimo de espera do sensor
+  // O valor retornado por sensor.min_delay é em microssegundos, então dividimos por 1000
+  // para obter milissegundos, que é o formato esperado por delay().
+  delayMS = sensor.min_delay / 1000;
 }
 
 void loop()
 {
-  delay(delayMS);                               // atraso entre as medições
-  sensors_event_t event;                        // inicializa o evento da Temperatura
-  dht.temperature().getEvent(&event);           // faz a leitura da Temperatura
-  if (isnan(event.temperature))                 // se algum erro na leitura
+  delay(delayMS);
+  // Inicializa o evento (estrutura que contém dados lidos) de temperatura e umidade
+  sensors_event_t event;
+
+  // Realiza a leitura dos dados de temperatura. Se for inválido (x), mostra uma mensagem de erro. 
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature))
   {
     Serial.println("Erro na leitura da Temperatura!");
   }
-  else                                          // senão
+  else
   {
-    Serial.print("Temperatura: ");              // imprime a Temperatura
+    Serial.print("Temperatura: ");
     Serial.print(event.temperature);
     Serial.println(" *C");
   }
-  dht.humidity().getEvent(&event);              // faz a leitura de umidade
-  if (isnan(event.relative_humidity))           // se algum erro na leitura
+
+  // Realiza a leitura dos dados de umidade. Se for inválido (x), mostra uma mensagem de erro. 
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
   {
     Serial.println("Erro na leitura da Umidade!");
   }
-  else                                          // senão
+  else
   {
-    Serial.print("Umidade: ");                  // imprime a Umidade
+    Serial.print("Umidade: ");
     Serial.print(event.relative_humidity);
     Serial.println("%");
   }
