1f1da22ca9f2a4049fbc82dff89362c36b9a9a3b
Rafaela Savaris <savarisf.rafaela@gmail.com>
2025-09-12 16:34:50 -0300

    server recebe informacoes do esp

--- software/server.js
+++ software/server.js
@@ -1,19 +1,14 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
-import { criarTabela, inserirLeitura, buscarHistorico } from "./database.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 const PORT = 4000;
+app.use(express.json());
 
 let dadosESP = {};
 
-// Cria tabela ao iniciar
-criarTabela().then(() => console.log("Tabela criada ou já existente"));
-
-// Busca dados externos
 app.get("/events", async (req, res) => {
   try {
     const response = await fetch("https://in-the-sky.org/rss.php");
@@ -24,34 +19,17 @@ app.get("/events", async (req, res) => {
   }
 });
 
-// Recebe dados do ESP32
-app.post("/dados", async (req, res) => {
+// rota para receber dados do ESP32
+app.post("/dados", (req, res) => {
   dadosESP = req.body;
-  try {
-    await inserirLeitura(dadosESP);  // salva no MySQL
-    res.send("OK");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao salvar no MySQL");
-  }
+  res.send("OK");
 });
 
-// Retorna último dado
+// permite acesso aos dados do ESP32
 app.get("/dados", (req, res) => {
   res.json(dadosESP);
 });
 
-// Retorna histórico completo
-app.get("/historico", async (req, res) => {
-  try {
-    const historico = await buscarHistorico();
-    res.json(historico);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Erro ao buscar histórico");
-  }
-});
-
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor rodando na rede na porta ${PORT}`);
+  console.log("Servidor rodando na rede!");
 });
